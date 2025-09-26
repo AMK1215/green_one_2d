df3f7d94254eb09fd7ba322c6c3b339bfcc0a964
AMK1215 <aungmyokhaing1215@gmail.com>
2025-09-26 11:06:43 +0630

    lottery ticket admin site updated

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,10 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
+// Ensure lottery ticket pages are included in the build
+import './pages/admin/LotteryTickets/Index';
+import './pages/admin/LotteryTickets/Show';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
